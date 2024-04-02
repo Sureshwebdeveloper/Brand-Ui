4ea843f381c10df861d192f5f00aa9d2dbfe4288
Suresh <sureshb9a11460@gmail.com>
2024-04-02 17:24:35 +0530

    responsive design added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./**/*.{html,js}"],
-  theme: {
-    extend: {},
+export const content = ["./**/*.{html,js}"];
+export const theme = {
+  extend: {},
+  screens: {
+    'sm': { min: "400px", max: "640px" },
+
+    'md': { min: "641px", max: "1080px" },
+
+    'lg': { min: "1081px", max: "1280px" },
+
+    'xl': { min: "1281px", max: "1535px" },
+
+    '2xl': { min: "1536px", max: "2800px" },
   },
   plugins: [],
 };
